0f6b587daf135f9c75c685b20e0625f2ef25dd57
Kazi Ramisa Rifa <76848879+KaziRamisaRifa@users.noreply.github.com>
2022-02-28 08:47:00 +0600

    Index implementation

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/index.css">
+
+<title>Hometrieve</title>
+  </head>
+  <body>
+
+  <!--Section - Carousel-->
+    <div id="demo" class="carousel slide" data-ride="carousel">
+      <ul class="carousel-indicators">
+        <li data-target="#demo" data-slide-to="0" class="active"></li>
+        <li data-target="#demo" data-slide-to="1"></li>
+        <li data-target="#demo" data-slide-to="2"></li>
+      </ul>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="image/index_c_1.jpg" alt="Los Angeles" width="1100" height="500">
+          <div class="carousel-caption">
+            <h1 class="text-white display-6 font-weight-bold">Rent a house</h1>
+            <p class="text-white font-weight-bold">Get your desired accommodation!</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="image/" alt="Chicago" width="1100" height="500">
+          <div class="carousel-caption">
+            <h1 class="text-white display-6 font-weight-bold">P</h1>
+            <p class="text-white font-weight-bold">A</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="image/" alt="New York" width="1100" height="500">
+          <div class="carousel-caption">
+            <h1 class="text-white display-6 font-weight-bold">g</h1>
+            <p class="text-white font-weight-bold">\</p>
+          </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#demo" data-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+      </a>
+      <a class="carousel-control-next" href="#demo" data-slide="next">
+        <span class="carousel-control-next-icon"></span>
+      </a>
+    </div>
+
+<!--Section - Features Option(Rent, Buy, Sell)-->
+  <section>
+      <div class="blog">
+        <div class="container">
+          <h1 class="text-center font-weight-bold text-dark text-capitalize pt-5">Check Out Our Services</h1>
+          <hr class="w-25 pt-4">
+          <div class="row mb-5">
+
+            <div class="col-md-4 col-lg-4 col-sm-12">
+              <div class="card">
+                <div class="card-img">
+                  <img src="image/rent.jpg" class="img-fluid">
+                </div>
+                <div class="card-body">
+                  <h4 class="card-title">Rent</h4>
+                  <p class="card-text">
+                  A reliable browsing experience to make easy for everyone to find desired houses for rent at any location in any time.
+                  </p>
+                </div>
+                <div class="card-footer">
+                  <div class="text-center">
+                  <button class="btn bg-primary text-white text-left" onclick="window.location =''">Rent a house</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4 col-lg-4 col-sm-12">
+              <div class="card">
+                <div class="card-img">
+                  <img src="image/buy.jpg" class="img-fluid">
+                </div>
+                <div class="card-body">
+                  <h4 class="card-title">Buy</h4>
+                  <p class="card-text">
+                    Anyone get their desired house or land by one click. You can find your convinient place to buy houses and land.  
+                  </p>
+                </div>
+                <div class="card-footer">
+                  <div class="text-center">
+                  <button class="btn bg-primary text-white text-left" onclick="window.location =''">Buy house or land</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4 col-lg-4 col-sm-12">
+              <div class="card">
+                <div class="card-img">
+                  <img src="image/sell.jpg" class="img-fluid">
+                </div>
+                <div class="card-body">
+                  <h4 class="card-title">Sell</h4>
+                  <p class="card-text">
+                  An user can become a seller and sell their lands and houses here at their own price. They can also post adds.
+                  </p>
+                </div>
+                <div class="card-footer">
+                  <div class="text-center">
+                  <button class="btn bg-primary text-white text-left" onclick="window.location =''">Sell house or land</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </section>
+  
+
+
+
+
+  </body>
+</html>
