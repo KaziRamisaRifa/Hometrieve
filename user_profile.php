b86d0f61fc4452f7dfed117267af5feea855b10d
Kazi Ramisa Rifa <76848879+KaziRamisaRifa@users.noreply.github.com>
2022-03-07 12:49:17 +0600

    Update user_profile.php

--- user_profile.php
+++ user_profile.php
@@ -10,22 +10,29 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="assets/css/user_profile.css">
 
     <title>User | Profile</title>
     <style>
-
+body
+{
+  background-image: url('assets/image/up_bg.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+}
 </style>
 </head>
 <body>
   <div class="container">
-  <div class="text-center">
+  
   <h2><strong>Welcome Ramisa Rifa!</strong></h2>
-  <a class="btn btn-primary" href="donor_login.php" role="button">Log Out</a>
-  </div>  
+  <a class="button-62" href="donor_login.php" role="button">Log Out</a>
+  
   
   <div class="row ">
-  <div class="w-50 mx-auto">
-    <div class="col-md-10 col-lg-10 col-sm-12">
+  
+    <div class="col-md-5 col-lg-5 col-sm-12">
     <br>
       <ul class="nav nav-tabs">
         <li class="active"><a data-toggle="tab" href="#about">About</a></li>
@@ -108,53 +115,25 @@
     </div>
     <div class="col-md-2">
       <br>
-      <a class="btn btn-primary" href="donor_login.php" role="button">Edit Profile</a>
+      <a class="button-62" href="donor_login.php" role="button">Edit Profile</a>
     </div>
-  </div>
+    <div class="col-md-5 col-lg-5 col-sm-12">
+  <div class="row pt-5">
+    
+    <button class="button-63" role="button" onclick="window.location =''"><i class="fa fa-search"></i> Search a house for rent now!</button>
 </div>
 <div class="row pt-5">
-<div class="w-50 mx-auto">
-<div class="text-center">
-<button class="btn bg-primary text-white text-left" onclick="window.location =''"><i class="fa fa-search"></i> Search a house for rent now!</button>
-</div>
-</div>
-</div>
-<div class="row pt-5">
-<div class="w-50 mx-auto">
-    <div class="col-md-6 col-lg-6 col-sm-12 ml-auto">
-      <div class="card">
-        <div class="card-img">
-          <img src="assets/image/buy.jpg" class="img-fluid">
-        </div>
-        <div class="card-body">
-          <h4 class="card-title">House</h4>
-          <p class="card-text">Anyone get their desired house or land by one click. You can find your convinient place to buy houses and land.</p>
-        </div>
-        <div class="card-footer">
-          <div class="text-center">
-            <button class="btn bg-primary text-white text-left" onclick="window.location ='add_houses.php'">Add House</button>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-6 col-lg-6 col-sm-12 ml-auto">
-      <div class="card">
-        <div class="card-img">
-          <img src="assets/image/buy.jpg" class="img-fluid">
-        </div>
-        <div class="card-body">
-          <h4 class="card-title">Land</h4>
-          <p class="card-text">Anyone get their desired house or land by one click. You can find your convinient place to buy houses and land.</p>
-        </div>
-        <div class="card-footer">
-          <div class="text-center">
-            <button class="btn bg-primary text-white text-left" onclick="window.location ='add_lands.php'">Add Land</button>
-          </div>
-        </div>
-      </div>
-    </div>
+      <button class="button-63" onclick="window.location ='add_houses.php'"  role="button"><i class="fa fa-home"> Add House +  </i>  </button>
+      <br>
+      <button class="button-63" onclick="window.location ='add_lands.php'"  role="button"><i class="fa fa-tree"> Add Land + </i></button>
+    
   </div>
-  </div>
+  
+
+</div>
+  
+</div>
+
   
 </div>
 </body>
