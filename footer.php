0e23a7e9b997027f4bb7fc8fbed5beebe2887663
KhalidBrinto <kbsbrinto@gmail.com>
2022-03-07 13:15:59 +0600

    header.php and footer.php page added

---
+++ footer.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Hometrieve</title>
+    <meta content="Hometrieve  is a web-based platform that will provide a way for the user to search a house for rent and both house buyer and seller can deal under this system In Bangladesh." name="description">
+    <meta content="NSU, ACM, Student, Chapter, SC" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/img/acm-logo.png" rel="icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- CSS CDN Files -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css" />
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+
+    <!-- Main CSS File -->
+    <link href="./css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer">
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-3 col-md-6 footer-contact">
+                        <h3>Home<span>trieve</span></h3>
+                        <p>
+                            We aim to achieve a <br> convenient way for<strong>
+                                renting accommodation at best rates <br></strong>
+                            In Bangladesh.
+                        </p>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 footer-links">
+                        <h4>Useful Links</h4>
+                        <ul>
+                            <li><i class="bx bx-chevron-right"></i> <a href="https://www.acm.org/education/" target="_blank">Dummy text</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="https://www.acm.org/publications/digital-library/" target="_blank">Dummy text</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 footer-links">
+                        <br>
+                        <br>
+                        <ul>
+                            <li><i class="bx bx-chevron-right"></i> <a href="http://www.northsouth.edu/" target="_blank">Dummy text</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="http://ece.northsouth.edu/" target="_blank">Dummy text</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="about.html" target="_blank">About US</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 footer-links">
+                        <h4>Our Social Networks</h4>
+                        <p>Get the latest updates from our<br>social profiles!</p>
+                        <div class="social-links mt-3">
+                            <a href="" target="_blank" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                            <a href="" target="_blank" class="github"><i class="bx bxl-github"></i></a>
+                            <a href="" target="_blank" class="facebook"><i class="bx bxl-facebook"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container py-2">
+            <div class="credits">
+                <p>Made with ❤️ by <a href="webmasters.php" target="_blank">WebMasters</a></p>
+            </div>
+        </div>
+    </footer><!-- End Footer -->
+
+    <!-- <div id="preloader"></div> -->
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- JS CDN Files -->
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/mcstudios/glightbox/dist/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+    <!-- Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Hometrieve</title>
+    <meta content="Hometrieve  is a web-based platform that will provide a way for the user to search a house for rent and both house buyer and seller can deal under this system In Bangladesh." name="description">
+    <meta content="NSU, ACM, Student, Chapter, SC" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/img/acm-logo.png" rel="icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- CSS CDN Files -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css" />
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+
+    <!-- Main CSS File -->
+    <link href="./css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <!-- ======= Header ======= -->
+    <header id="header" class="d-flex align-items-center">
+        <div class="container d-flex align-items-center justify-content-between">
+            <a href="index.php" class="logo"><img src="assets/image/logo1c.jpeg" alt="Hometrieve"></a>
+
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li class="dropdown"><a class="nav-link scrollto active" href="#team"><span>Home</span> <i class="bi bi-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="">Rent Home</a></li>
+                            <li><a href="">Buy Houses</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown"><a href="#team"><span>Lands</span> <i class="bi bi-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="">Buy Land</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown"><a href="#team"><span>Add property</span> <i class="bi bi-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="add_houses.php">Add Houses</a></li>
+                            <li><a href="add_lands.php">Add Lands</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="nav-link scrollto" href="#">Contact</a></li>
+                    <li><a class="nav-link scrollto" href="#">Register</a></li>
+
+                    <?php
+                    $logged_in = FALSE;
+
+                    if (!isset($username)) echo '<li><a class="nav-link scrollto" href="login.php">Login</a></li>';
+                    else echo '<li><a href="logout.php">' . $username . '</a></li>';
+                    ?>
+
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav><!-- .navbar -->
+
+        </div>
+    </header><!-- End Header -->
+
+    <!-- <div id="preloader"></div> -->
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- JS CDN Files -->
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/mcstudios/glightbox/dist/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+    <!-- Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
