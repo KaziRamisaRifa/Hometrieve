3c13f6f49798c52d644d5ade36d428140b4f3f0f
Kazi Ramisa Rifa <76848879+KaziRamisaRifa@users.noreply.github.com>
2022-03-07 12:49:12 +0600

    Create view_houses.php

---
+++ view_houses.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+
+    <title>View Houses</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <!--script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+body
+{
+  background-image: url('assets/image/ambulancesigncover.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+}
+img {
+    height: 170px;
+    width: 200px
+}
+</style>
+   </head>
+   
+  
+
+  
+   <body>
+   <h1 class="text-center text-dark text-capitalize pt-5">Houses List</h1>
+                <hr class="w-25 pt-3">
+   <div class="container">
+    <div class="row">
+        <div class="col-lg-8 mx-auto">
+            <ul class="list-group shadow">
+                <li class="list-group-item">
+                    <div class="media align-items-lg-center flex-column flex-lg-row p-3">
+                        <div class="media-body order-2 order-lg-1">
+                            <h5><strong>Bashundhara | 1800 sqft | For Rent</strong></h5>
+                            <p class="text-muted"><i class="fa fa-bed">  </i> 2 Beds | <i class="fa fa-bath"></i> 2 Baths | <i class="fa fa-trello">  </i> 1 Balcony | <i class="fa fa-arrow-up"></i> 2nd Floor</p>
+                            <div class="d-flex align-items-center justify-content-between mt-1">
+                                <h6><strong><i class="fa fa-money" aria-hidden="true"></i> 64,999 BDT</strong></h6>
+                                <p><a href="favourite_list.php">View Details---></a></p>
+                            </div>
+                        </div>
+                        <img src="assets/image/signup_bg.jpg" alt="Generic placeholder image" width="200" class="ml-lg-5 order-1 order-lg-2">
+                    </div> 
+                </li> 
+                <li class="list-group-item">
+                    <div class="media align-items-lg-center flex-column flex-lg-row p-3">
+                        <div class="media-body order-2 order-lg-1">
+                            <h5><strong>Bashundhara | 1800 sqft | For Rent</strong></h5>
+                            <p class="text-muted"><i class="fa fa-bed">  </i> 2 Beds | <i class="fa fa-bath"></i> 2 Baths | <i class="fa fa-trello">  </i> 1 Balcony | <i class="fa fa-arrow-up"></i> 2nd Floor</p>
+                            <div class="d-flex align-items-center justify-content-between mt-1">
+                                <h6><strong><i class="fa fa-money" aria-hidden="true"></i> 64,999 BDT</strong></h6>
+                                <p><a href="favourite_list.php">View Details---></a></p>
+                            </div>
+                        </div>
+                        <img src="assets/image/signup_bg.jpg" alt="Generic placeholder image" width="200" class="ml-lg-5 order-1 order-lg-2">
+                    </div> 
+                </li>
+                <!-- list group item -->
+                <li class="list-group-item">
+                    <div class="media align-items-lg-center flex-column flex-lg-row p-3">
+                        <div class="media-body order-2 order-lg-1">
+                            <h5><strong>Bashundhara | 1800 sqft | For Rent</strong></h5>
+                            <p class="text-muted"><i class="fa fa-bed">  </i> 2 Beds | <i class="fa fa-bath"></i> 2 Baths | <i class="fa fa-trello">  </i> 1 Balcony | <i class="fa fa-arrow-up"></i> 2nd Floor</p>
+                            <div class="d-flex align-items-center justify-content-between mt-1">
+                                <h6><strong><i class="fa fa-money" aria-hidden="true"></i> 64,999 BDT</strong></h6>
+                                <p><a href="favourite_list.php">View Details---></a></p>
+                            </div>
+                        </div>
+                        <img src="assets/image/signup_bg.jpg" alt="Generic placeholder image" width="200" class="ml-lg-5 order-1 order-lg-2">
+                    </div> 
+                </li>
+                <li class="list-group-item">
+                    <div class="media align-items-lg-center flex-column flex-lg-row p-3">
+                        <div class="media-body order-2 order-lg-1">
+                            <h5><strong>Bashundhara | 1800 sqft | For Rent</strong></h5>
+                            <p class="text-muted"><i class="fa fa-bed">  </i> 2 Beds | <i class="fa fa-bath"></i> 2 Baths | <i class="fa fa-trello">  </i> 1 Balcony | <i class="fa fa-arrow-up"></i> 2nd Floor</p>
+                            <div class="d-flex align-items-center justify-content-between mt-1">
+                                <h6><strong><i class="fa fa-money" aria-hidden="true"></i> 64,999 BDT</strong></h6>
+                                <p><a href="favourite_list.php">View Details---></a></p>
+                            </div>
+                        </div>
+                        <img src="assets/image/signup_bg.jpg" alt="Generic placeholder image" width="200" class="ml-lg-5 order-1 order-lg-2">
+                    </div> 
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+  </body>
+</html>
