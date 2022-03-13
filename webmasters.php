51127a4f5dfecf63a8be6135c92a882c1d80a23c
KhalidBrinto <kbsbrinto@gmail.com>
2022-03-13 23:33:43 +0600

    WebMasters page modified

--- webmasters.php
+++ webmasters.php
@@ -28,10 +28,16 @@
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
 
     <!-- Template Main CSS File -->
-    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
     <!-- Custom CSS File -->
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="assets/css/teams.css">
 
+    <!-- =======================================================
+  * Template Name: BizLand - v3.0.1
+  * Template URL: https://bootstrapmade.com/bizland-bootstrap-business-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
 </head>
 
 <body>
@@ -39,48 +45,37 @@
     <!-- ======= Header ======= -->
     <header id="header" class="d-flex align-items-center">
         <div class="container d-flex align-items-center justify-content-between">
-            <a href="home.html" class="logo"><img src="assets/img/acm-logo.png" alt="NSU ACM Student Chapter"></a>
+            <a href="index.php" class="logo"><img src="assets/image/logo1c.jpeg" alt="Hometrieve"></a>
 
             <nav id="navbar" class="navbar">
                 <ul>
-                    <li><a class="nav-link scrollto " href="home.html">Home</a></li>
-                    <li><a class="nav-link scrollto" href="update.html">Updates</a></li>
-                    <li><a class="nav-link scrollto" href="event.html">Events</a></li>
-                    <li><a class="nav-link scrollto" href="about.html">About</a></li>
-                    <li class="dropdown"><a href="#team"><span>Core Body</span> <i class="bi bi-chevron-down"></i></a>
+                    <li class="dropdown"><a class="nav-link scrollto active" href="#team"><span>Home</span> <i class="bi bi-chevron-down"></i></a>
                         <ul>
-                            <li><a href="executive-body.html">Executive Body</a></li>
-                            <li><a href="sub-executive.html">Sub Executive</a></li>
-                            <li><a href="incharge.html">In Charge</a></li>
-                            <li><a href="ex-executives.html">Ex-Executive Body</a></li>
-                            <li><a href="ex-faculty-sponsor.html">Ex-Faculty Sponsor</a></li>
+                            <li><a href="">Rent Home</a></li>
+                            <li><a href="">Buy Houses</a></li>
                         </ul>
                     </li>
-                    <li class="dropdown"> <a class="active" href="#faq"><span>Team</span> <i class="bi bi-chevron-down"></i></a>
+                    <li class="dropdown"><a href="#team"><span>Lands</span> <i class="bi bi-chevron-down"></i></a>
                         <ul>
-                            <li><a href="team-promotions.html">Team Promotions</a></li>
-                            <li><a href="team-corporate.html"> Team Corporate</a></li>
-                            <li><a href="team-publications.html"> Team Publications</a></li>
-                            <li><a href="team-provision.html">Team Provision</a></li>
-                            <li><a href="floating-team.html">Floating Teams</a>
-                            </li>
+                            <li><a href="">Buy Land</a></li>
                         </ul>
                     </li>
-                    <li><a class="nav-link scrollto " href="membership.html">Membership</a></li>
-                    <li><a class="nav-link scrollto" href="verify-cert">Certificate</a></li>
-                    <!-- Dark mode -->
-                    <li>
-                        <form class="form">
-                            <td width="100">
-                                <div class="toggle">
-                                    <input id="on" type="radio" value="on" name="radio" onclick="changeState()">
-                                    <input id="off" type="radio" value="off" name="radio" onclick="changeState()">
-                                    <div class="toggle__pointer"></div>
-                                </div>
-                            </td>
-                        </form>
-                        <!--- <button onclick="toggleTheme('dark');"  >Dark</button><button onclick="toggleTheme('light');">Light Mode</button> -->
+                    <li class="dropdown"><a href="#team"><span>Add property</span> <i class="bi bi-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="add_houses.php">Add Houses</a></li>
+                            <li><a href="add_lands.php">Add Lands</a></li>
+                        </ul>
                     </li>
+                    <li><a class="nav-link scrollto" href="#">Contact</a></li>
+                    <li><a class="nav-link scrollto" href="#">Register</a></li>
+
+                    <?php
+                    $logged_in = FALSE;
+
+                    if (!isset($username)) echo '<li><a class="nav-link scrollto" href="login.php">Login</a></li>';
+                    else echo '<li><a href="logout.php">' . $username . '</a></li>';
+                    ?>
+
                 </ul>
                 <i class="bi bi-list mobile-nav-toggle"></i>
             </nav><!-- .navbar -->
@@ -95,66 +90,76 @@
             <div class="container" data-aos="fade-up">
 
                 <div class="section-title">
-                    <h2>WebMasters</h2>
+                    <h3>Web <span>Masters</span></h3>
                 </div>
+                <!-- <div class="section-title mb-1">
+          <h2>Sub Executive</h2>
+        </div> -->
+                <!-- <div class="section-title" data-aos="fade-up">
+          <h3 style="text-align: center;">Webteam <span>Leads</span></h3>
+        </div> -->
 
                 <div class="row justify-content-center">
-                    <div class="col-lg-6 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
                         <div class="member">
                             <div class="member-img">
-                                <img src="assets/img/team/Sudipta Bhatta Vice Chair.jpg" class="img-fluid" alt="">
-                                <!-- <div class="social">
-                          <a href="#" target="_blank" class="facebook"><i class="bx bxl-facebook"></i></a>
-                          <a href="#" target="_blank" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-                      </div> -->
+                                <img src="assets/image/team/Khalid Bin Shafiq.jpg" class="img-fluid" alt="">
+                                <div class="social">
+                                    <a href="#" target="_blank" class="facebook"><i class="bx bxl-facebook"></i></a>
+                                    <a href="#" target="_blank" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                                </div>
                             </div>
                             <div class="member-info">
                                 <h4>Khalid Bin Shafiq</h4>
+                                <span>Developer</span>
                             </div>
                         </div>
                     </div>
 
-                    <div class="col-lg-6 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
                         <div class="member">
                             <div class="member-img">
-                                <img src="assets/img/team/S. M. Sajid Hasan Shanta (Sub Eb Team Promotions).jpg" class="img-fluid" alt="">
-                                <!-- <div class="social">
-                              <a href="#" target="_blank" class="facebook"><i class="bx bxl-facebook"></i></a>
-                              <a href="#" target="_blank" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-                          </div> -->
+                                <img src="assets/image/team/Khalid Bin Shafiq.jpg" class="img-fluid" alt="">
+                                <div class="social">
+                                    <a href="#" target="_blank" class="facebook"><i class="bx bxl-facebook"></i></a>
+                                    <a href="#" target="_blank" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                                </div>
                             </div>
                             <div class="member-info">
-                                <h4>Kazi ramisa Rifa</h4>
+                                <h4>Kazi Ramisa Rifa</h4>
+                                <span>Developer</span>
                             </div>
                         </div>
                     </div>
 
-                    <div class="col-lg-6 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
                         <div class="member">
                             <div class="member-img">
-                                <img src="assets/img/team/Khalid Bin Shafiq.jpg" class="img-fluid" alt="">
-                                <!-- <div class="social">
-                              <a href="#" target="_blank" class="facebook"><i class="bx bxl-facebook"></i></a>
-                              <a href="#" target="_blank" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-                          </div> -->
+                                <img src="assets/image/team/Khalid Bin Shafiq.jpg" class="img-fluid" alt="">
+                                <div class="social">
+                                    <a href="#" target="_blank" class="facebook"><i class="bx bxl-facebook"></i></a>
+                                    <a href="#" target="_blank" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                                </div>
                             </div>
                             <div class="member-info">
                                 <h4>Sabiha Hossain</h4>
+                                <span>Developer</span>
                             </div>
                         </div>
                     </div>
 
-                    <div class="col-lg-6 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
                         <div class="member">
                             <div class="member-img">
-                                <img src="assets/img/team/Khalid Bin Shafiq.jpg" class="img-fluid" alt="">
-                                <!-- <div class="social">
-                              <a href="#" target="_blank" class="facebook"><i class="bx bxl-facebook"></i></a>
-                              <a href="#" target="_blank" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-                          </div> -->
+                                <img src="assets/image/team/Khalid Bin Shafiq.jpg" class="img-fluid" alt="">
+                                <div class="social">
+                                    <a href="#" target="_blank" class="facebook"><i class="bx bxl-facebook"></i></a>
+                                    <a href="#" target="_blank" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                                </div>
                             </div>
                             <div class="member-info">
                                 <h4>Shabab Irtiza Ahammad</h4>
+                                <span>Developer</span>
                             </div>
                         </div>
                     </div>
@@ -172,10 +177,10 @@
                 <div class="row">
 
                     <div class="col-lg-3 col-md-6 footer-contact">
-                        <h3>NSU <span>ACM SC</span></h3>
+                        <h3>Home<span>trieve</span></h3>
                         <p>
-                            The First Student Chapter <br>Of <strong>The
-                                Association For Computing <br>Machineries</strong>
+                            We aim to achieve a <br> convenient way for<strong>
+                                renting accommodation at best rates <br></strong>
                             In Bangladesh.
                         </p>
                     </div>
@@ -183,11 +188,8 @@
                     <div class="col-lg-3 col-md-6 footer-links">
                         <h4>Useful Links</h4>
                         <ul>
-                            <li><i class="bx bx-chevron-right"></i> <a href="https://www.acm.org/education/" target="_blank">ACM
-                                    Education</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="https://www.acm.org/publications/digital-library/" target="_blank">ACM Digital Library</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="https://www.acm.org/code-of-ethics/" target="_blank">ACM
-                                    Code of Ethics</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="https://www.acm.org/education/" target="_blank">Dummy text</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="https://www.acm.org/publications/digital-library/" target="_blank">Dummy text</a></li>
                         </ul>
                     </div>
 
@@ -195,61 +197,28 @@
                         <br>
                         <br>
                         <ul>
-                            <li><i class="bx bx-chevron-right"></i> <a href="http://www.northsouth.edu/" target="_blank">NSU
-                                    Website</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="http://ece.northsouth.edu/" target="_blank">NSU ECE
-                                    Website</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="http://www.northsouth.edu/" target="_blank">Dummy text</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="http://ece.northsouth.edu/" target="_blank">Dummy text</a></li>
                             <li><i class="bx bx-chevron-right"></i> <a href="about.html" target="_blank">About US</a></li>
                         </ul>
-                        <!--
-            ACM Education
-            https://www.acm.org/education/
-
-            ACM Digital Library
-            https://www.acm.org/publications/digital-library/
-
-            ACM Code of Ethics
-            https://www.acm.org/code-of-ethics/
-
-            NSU Website
-            http://www.northsouth.edu/
-
-            NSU ECE Website
-            http://ece.northsouth.edu/
-
-            About US
-            -->
                     </div>
 
                     <div class="col-lg-3 col-md-6 footer-links">
                         <h4>Our Social Networks</h4>
                         <p>Get the latest updates from our<br>social profiles!</p>
                         <div class="social-links mt-3">
-                            <a href="https://www.linkedin.com/company/nsuacmc/" target="_blank" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-                            <a href="https://github.com/NSU-ACM-SC/" target="_blank" class="github"><i class="bx bxl-github"></i></a>
-                            <a href="https://tinyurl.com/y228boc9/" target="_blank" class="youtube"><i class="bx bxl-youtube"></i></a>
-                            <a href="https://www.facebook.com/nsuacmc/" target="_blank" class="facebook"><i class="bx bxl-facebook"></i></a>
-                            <a href="https://medium.com/nsuacmsc/" target="_blank" class="medium"><i class="bx bxl-medium"></i></a>
-                            <!--
-              LinkedIn: https://www.linkedin.com/company/nsuacmc
-              GitHub: https://github.com/NSU-ACM-SC
-              Youtube:https://tinyurl.com/y228boc9
-              Facebook: https://www.facebook.com/nsuacmc
-              Medium: https://medium.com/nsuacmsc
-              -->
+                            <a href="" target="_blank" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                            <a href="" target="_blank" class="github"><i class="bx bxl-github"></i></a>
+                            <a href="" target="_blank" class="facebook"><i class="bx bxl-facebook"></i></a>
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>
 
-        <div class="container py-4">
-            <div class="copyright">
-                &copy; Copyright <strong><span>NSU ACM SC</span></strong>. All Rights Reserved
-            </div>
+        <div class="container py-2">
             <div class="credits">
-                Developed by <a href="web-team.html">NSU ACM SC Web Team</a>
+                <p>Made with ❤️ by <a href="webmasters.php" target="_blank">WebMasters</a></p>
             </div>
         </div>
     </footer><!-- End Footer -->
